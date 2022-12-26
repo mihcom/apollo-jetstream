8a0f1c5a7c3f9ee1ae1a9f4521d9e02be4004a0e
Mykhaylo Gomonyuk <mykhaylo.gomonyuk@valtech.com>
2022-12-26 21:15:35 +0100

    Added animated timeline

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,5 +1,5 @@
 ﻿<template>
-  <svg height="350" width="1100"></svg>
+  <svg :width="windowWidth" class="timeline" height="350"></svg>
 </template>
 
 <script>
@@ -7,57 +7,40 @@ import * as d3 from 'd3'
 
 export default {
   name: 'Timeline',
-  mounted() {
-    const data = [
-      {
-        year: 2010,
-        value: 10
-      },
-      {
-        year: 2011,
-        value: 14
-      },
-      {
-        year: 2017,
-        value: 18
-      }
-    ]
 
-    const svg = d3.select('svg'),
+  data() {
+    return {
+      windowWidth: window.innerWidth - 50
+    }
+  },
+
+  mounted() {
+    const svg = d3.select('svg.timeline'),
       width = svg.attr('width'),
       height = svg.attr('height') - 30,
-      xScale = d3.scaleBand().range([0, width]).padding(0.4),
+      xScale = d3.scaleTime().range([0, width]),
       yScale = d3.scaleLinear().range([height, 0]),
       g = svg.append('g').attr('transform', 'translate(' + 30 + ',' + 10 + ')')
 
-    xScale.domain(data.map((d) => d.year))
-    yScale.domain([0, d3.max(data, (d) => d.value)])
+    xScale.domain(lastFiveMinutes())
+    yScale.domain([0, 20])
 
     g.append('g')
+      .attr('class', 'axis axis--x')
       .attr('transform', 'translate(0,' + height + ')')
       .call(d3.axisBottom(xScale))
 
-    g.append('g').call(
-      d3.axisLeft(yScale).tickFormat(function (d) {
-        return '$' + d
-      })
-    )
+    g.append('g').attr('class', 'axis axis--y').call(d3.axisLeft(yScale))
 
-    g.selectAll('.bar')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', function (d) {
-        return xScale(d.year)
-      })
-      .attr('y', function (d) {
-        return yScale(d.value)
-      })
-      .attr('width', xScale.bandwidth())
-      .attr('height', function (d) {
-        return height - yScale(d.value)
-      })
+    setInterval(() => {
+      xScale.domain(lastFiveMinutes())
+      d3.select('.axis--x').transition().call(d3.axisBottom(xScale))
+    }, 1000)
+
+    function lastFiveMinutes() {
+      const now = Date.now()
+      return [now - 5 * 60 * 1000, now]
+    }
   }
 }
 </script>

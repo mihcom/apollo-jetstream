dd7a7ff9d1cd09ed53ac209820c37369c0e91046
Mykhaylo Gomonyuk <mykhaylo.gomonyuk@valtech.com>
2022-12-28 19:30:30 +0100

    Added colors

--- src/api/nats.js
+++ src/api/nats.js
@@ -1,6 +1,5 @@
-import { AckPolicy, connect, DeliverPolicy, ReplayPolicy, nanos, createInbox } from 'nats.ws'
+import { AckPolicy, connect, DeliverPolicy, ReplayPolicy, createInbox } from 'nats.ws'
 import moment from 'moment'
-import { v4 as uuidv4 } from 'uuid'
 
 const serverUri = 'ws://localhost:443'
 
@@ -17,25 +16,23 @@ async function getStreams() {
         deliver_subject: createInbox(), // specify subject to make this consumer a push consumer
         description: 'apollo-jetstream debug consumer',
         opt_start_time: moment().add(-30, 'minutes').toISOString(), // start 5 minutes ago
-        replay_policy: ReplayPolicy.Instant, // get messages as soon as possible
-        inactive_threshold: nanos(moment.duration(10, 'seconds').asMilliseconds())
+        replay_policy: ReplayPolicy.Instant // get messages as soon as possible
       },
       consumerOptions = {
         config: consumerConfiguration,
         stream: stream.config.name
       }
 
-    // const subscription = await js.subscribe('>', consumerOptions)
+    stream.messages = []
+
     const subscription = await js.subscribe('>', consumerOptions)
 
-    const done = (async () => {
-      for await (const m of subscription) {
-        console.log(`[${m.seq}] ${m.subject} ${m.data}`)
-        m.ack()
+    ;(async () => {
+      for await (const message of subscription) {
+        stream.messages.push(message)
+        console.log(message)
       }
     })()
-
-    await done
   }
 
   return streams.sort((a, b) => a.config.name.localeCompare(b.config.name))
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -2,6 +2,7 @@
 import * as d3 from 'd3'
 import { onMounted, ref } from 'vue'
 import { useJetStreamStore } from '../stores/JetStream'
+import { millis } from 'nats.ws'
 
 const store = useJetStreamStore(),
   windowWidth = ref(window.innerWidth),
@@ -12,30 +13,79 @@ onMounted(async () => {
 
   const streams = store.streams,
     svg = d3.select('svg.timeline'),
+    leftMargin = 120,
     width = svg.attr('width'),
     height = svg.attr('height') - 30,
-    xScale = d3.scaleTime().domain(lastFiveMinutes()).range([0, width]),
+    xScale = d3
+      .scaleTime()
+      .domain(lastXMinites())
+      .range([0, width - leftMargin]),
     yScale = d3
       .scaleBand()
       .domain(streams.map(x => x.config.name))
       .range([0, height]),
-    g = svg.append('g').attr('transform', 'translate(' + 130 + ',' + 10 + ')')
+    accent = d3
+      .scaleOrdinal()
+      .domain(streams.map(x => x.config.name))
+      .range(d3.schemeSpectral[10]),
+    g = svg.append('g').attr('transform', 'translate(' + leftMargin + ',' + 10 + ')')
 
+  // add the x Axis (time)
   g.append('g')
     .attr('class', 'axis axis--x')
     .attr('transform', 'translate(0,' + height + ')')
     .call(d3.axisBottom(xScale))
 
-  g.append('g').attr('class', 'axis axis--y').call(d3.axisLeft(yScale))
+  // add the y Axis (streams)
+  g.append('g')
+    .attr('class', 'axis axis--y')
+    .call(d3.axisLeft(yScale))
+    .selectAll('text')
+    .style('fill', d => accent(d))
 
+  const timeBarrier = lastXMinites()[0],
+    data = streams
+      .flatMap(stream =>
+        stream.messages.map(message => ({
+          stream: stream.config.name,
+          subject: message.subject,
+          data: message.data,
+          id: `${message.subject}/${message.sid}`,
+          timestamp: message.info.timestampNanos
+        }))
+      )
+      .filter(message => message.timestamp > timeBarrier),
+    messageBarWidth = 20,
+    messageBarHeight = 10
+
+  g.selectAll('.message')
+    .data(data, d => d.id)
+    .join(enter =>
+      enter
+        .append('rect')
+        .attr('class', 'message')
+        .attr('x', d => xScale(millis(d.timestamp)))
+        .attr('y', d => yScale(d.stream) + messageBarHeight)
+        .attr('fill', d => accent(d.stream))
+        .attr('width', messageBarWidth)
+        .attr('height', messageBarHeight)
+        .append('title')
+        .text(d => d.subject)
+    )
+
+  // move the timeline each second
   setInterval(() => {
-    xScale.domain(lastFiveMinutes())
+    xScale.domain(lastXMinites())
     d3.select('.axis--x').transition().call(d3.axisBottom(xScale))
+
+    g.selectAll('.message')
+      .transition()
+      .attr('x', d => xScale(millis(d.timestamp)))
   }, 1000)
 
-  function lastFiveMinutes() {
+  function lastXMinites() {
     const now = Date.now()
-    return [now - 5 * 60 * 1000, now]
+    return [now - 30 * 60 * 1000, now]
   }
 })
 </script>
@@ -56,4 +106,12 @@ onMounted(async () => {
 
       &:hover
         color yellowgreen
+
+.message
+  stroke #747bff
+  margin-top 0.5em
+  cursor pointer
+
+  &:hover
+    fill #646cff
 </style>

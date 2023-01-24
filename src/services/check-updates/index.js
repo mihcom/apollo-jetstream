0919523a984a94a8e4eaad461fa7e77b8c1e9f2e
Mykhaylo Gomonyuk <mykhaylo.gomonyuk@valtech.com>
2023-01-24 20:59:34 +0100

    Added cumulative version info

--- src/services/check-updates/index.js
+++ src/services/check-updates/index.js
@@ -1,6 +1,7 @@
 ﻿import ReleaseInfo from './ReleaseInfo.vue'
 import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest'
 import { useToast } from 'vue-toastification'
+import { gt } from 'semver'
 
 export default async function checkUpdates(appVersion) {
   const octokit = new Octokit(),
@@ -10,13 +11,20 @@ export default async function checkUpdates(appVersion) {
     ignoreVersion = localStorage.getItem(ignoreVersionKey)
 
   if (lastRelease && lastRelease.tag_name !== appVersion && ignoreVersion !== lastRelease.tag_name) {
+    const newReleases = releases.filter(release => gt(release.tag_name, appVersion))
+
+    if (newReleases.length === 0) {
+      return
+    }
+
     const toast = useToast()
 
     toast.info(
       {
         component: ReleaseInfo,
         props: {
-          version: lastRelease
+          releases: newReleases,
+          appVersion: appVersion
         },
         listeners: {
           ignore: () => window.localStorage.setItem(ignoreVersionKey, lastRelease.tag_name)
